d7e6929c3f0ceaa350cd29451b61e503e18cc5b3
hellen truong <hellentruong123@gmail.com>
2024-11-27 22:59:49 +0100

    skapar routes/navigation mellan webbsidorna

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
+import RootLayout from "./layout/Root";
+import ClothesPage from "./pages/Clothes";
+import ShoesPage from "./pages/Shoes";
+import AccessoriesPage from "./pages/Accessories";
+import HomePage from "./pages/Home";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="clothes" element={<ClothesPage />} />
+        <Route path="shoes" element={<ShoesPage />} />
+        <Route path="accessories" element={<AccessoriesPage />} />
+      </Route>
+    )
+  );
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
