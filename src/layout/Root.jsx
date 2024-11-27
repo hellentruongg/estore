ff760b3870f8e204c5caebd837d37636d10f3f77
hellen truong <hellentruong123@gmail.com>
2024-11-28 00:24:19 +0100

    lägger in "navbar" komponenten

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 export default function RootLayout() {
   return (
     <>
-      <h1>Navigation bar</h1>
+      <Navbar />
       <Outlet />
     </>
   );
