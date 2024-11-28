2c45eac599c4d7c5042975f70e91facab2eb6279
hellen truong <hellentruong123@gmail.com>
2024-11-28 22:12:51 +0100

    skapar navbar komponent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,7 @@ export default function NavigationBar() {
     <>
       <Navbar fluid rounded>
         <Navbar.Brand href="/">
-          <img
-            src="/favicon.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Femme Logo"
-          />
+          <img src="#" className="mr-3 h-6 sm:h-9" alt="Femme Logo" />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Femme
           </span>
