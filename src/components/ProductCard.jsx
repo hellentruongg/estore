def34273fb9733e9a21f1cf7ee3ba233ab550f99
hellen truong <hellentruong123@gmail.com>
2024-11-29 11:33:58 +0100

    lägger in hårdkodad "Card" komponent från flowbite-react

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,3 +1,18 @@
+import { Card } from "flowbite-react";
+
 export default function ProductCard() {
-  return <></>;
+  return (
+    <>
+      <Card
+        className="max-w-sm"
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="/images/blog/image-1.jpg"
+      >
+        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p className="text-3xl font-bold text-gray-900 dark:text-white">$599</p>
+      </Card>
+    </>
+  );
 }
