e8f6726823c46d947d086b06075e0bb7797c396f
hellen truong <hellentruong123@gmail.com>
2024-12-06 10:12:23 +0100

    hårdkodar "CartItem" komponenten

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,3 +1,36 @@
+import { Button } from "flowbite-react";
+import { BsDashLg, BsPlusLg } from "react-icons/bs";
+
 export default function () {
-  return <></>;
+  return (
+    <>
+      <img src="#" alt="" />
+      <p>Product</p>
+
+      <br />
+
+      <p>Color</p>
+
+      <br />
+
+      <p>Size</p>
+
+      <br />
+
+      <div className="flex gap-2">
+        {/* styla knappstorleken, båda ska vara lika stora i bredd och höjd */}
+        <Button size="xs" outline color="gray">
+          <BsDashLg />
+        </Button>
+        <span>X</span>
+        <Button size="xs" outline color="gray">
+          <BsPlusLg />
+        </Button>
+      </div>
+
+      <br />
+
+      <p>Price</p>
+    </>
+  );
 }
