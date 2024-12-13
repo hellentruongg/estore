ab2f617706c6eef817fe0af59da7c6a6283e4444
hellen truong <hellentruong123@gmail.com>
2024-12-13 09:50:24 +0100

    skapar funktion för att ändra antalet produkter i varukorgen

--- src/components/CartItemCard.jsx
+++ src/components/CartItemCard.jsx
@@ -1,11 +1,49 @@
 import { Button } from "flowbite-react";
 import { BsDashLg, BsPlusLg } from "react-icons/bs";
+import { useRecoilState } from "recoil";
+import { cartState } from "../states/cart";
 
 export default function CartItemCard({ cartItem }) {
-  const { title, price, color, image } = cartItem.product;
+  const { id, title, price, color, image } = cartItem.product;
   const { size, amount } = cartItem;
 
   console.log(amount);
+
+  const [cart, setCart] = useRecoilState(cartState);
+
+  const foundCartItem = cart.find(
+    (item) => item.product.id === id && item.size === size
+  );
+
+  function increaseAmount() {
+    setCart(
+      cart.map((item) => {
+        if (item.product.id === id && item.size === size) {
+          return { ...item, amount: item.amount + 1 };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  function decreaseAmount() {
+    if (foundCartItem.amount <= 1) {
+      setCart(
+        cart.filter((item) => !(item.product.id === id && item.size === size))
+      );
+    } else {
+      setCart(
+        cart.map((item) => {
+          if (item.product.id === id && item.size === size) {
+            return { ...item, amount: item.amount - 1 };
+          } else {
+            return item;
+          }
+        })
+      );
+    }
+  }
 
   // Uppgift 0
   // hämta global variabel "cart"
@@ -39,11 +77,11 @@ export default function CartItemCard({ cartItem }) {
 
       <div className="flex gap-2">
         {/* styla knappstorleken, båda ska vara lika stora i bredd och höjd */}
-        <Button size="xs" outline color="gray">
+        <Button onClick={decreaseAmount} size="xs" outline color="gray">
           <BsDashLg />
         </Button>
         <span>{amount}</span>
-        <Button size="xs" outline color="gray">
+        <Button onClick={increaseAmount} size="xs" outline color="gray">
           <BsPlusLg />
         </Button>
       </div>
