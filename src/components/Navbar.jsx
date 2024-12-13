119e55673c27830847ae55f02bbb693d32dda89a
hellen truong <hellentruong123@gmail.com>
2024-12-13 10:08:21 +0100

    rättar till filnamnet till loggan i koden

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function NavigationBar() {
     <>
       <Navbar fluid rounded>
         <Navbar.Brand href="/">
-          <img src="/logo-1.png" className="mr-3 h-6 sm:h-9" alt="Femme Logo" />
+          <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="Femme Logo" />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Femme
           </span>
