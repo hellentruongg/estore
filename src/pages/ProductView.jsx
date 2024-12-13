d71508515b92a3c424322f95281d9da581ec1c3a
hellen truong <hellentruong123@gmail.com>
2024-12-13 10:15:16 +0100

    tar bort kommentarer

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -1,23 +1,17 @@
 import { Card, Button, Spinner } from "flowbite-react";
 import { useEffect } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { clothesState } from "../states/clothes";
 import { useState } from "react";
 import { cartState } from "../states/cart";
 
 export default function ProductViewPage() {
-  // OBS! useParams has to be used within a route with a path that has a dynamic parameter.
-
-  // 1.  use useParams in the ProductViewPage and then find the the id that match the url id
-  // 2. display the product information
-
   const { id } = useParams();
   const { pathname } = useLocation();
   const [clothes, setClothes] = useRecoilState(clothesState);
   const [cart, setCart] = useRecoilState(cartState);
   const [product, setProduct] = useState(null);
-  // const [sizeText, setSizeText] = useState("Choose size:");
   const [size, setSize] = useState("");
   const [selectedSize, setSelectedSize] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
@@ -49,8 +43,6 @@ export default function ProductViewPage() {
 
   function createSizeButton(size) {
     return (
-      // varje knapp har en onClick funktion som får props (size)
-      // onClick funktionen ändrar texten "choose size:" (se nedan)
       <Button key={size} onClick={() => handleSize(size)} outline color="gray">
         {size}
       </Button>
@@ -58,7 +50,6 @@ export default function ProductViewPage() {
   }
 
   function handleSize(size) {
-    // setSizeText(`Chosen size: ${size}`);
     setSize(size);
     setSelectedSize(true);
     setErrorMessage("");
@@ -67,7 +58,7 @@ export default function ProductViewPage() {
   function addToCart() {
     if (!selectedSize) {
       setErrorMessage("Please select a size before adding to cart.");
-      return; // Stop further execution
+      return;
     }
 
     const newCartItem = {
@@ -113,7 +104,7 @@ export default function ProductViewPage() {
           Color: <span>{color}</span>
         </p>
         <div>color</div>
-        {/* När man väljer storlek: ändra "choose size:" till "chosen size:" och lägg till vald storlek. Ex. Chosen size: XS. */}
+
         <p className="text-l text-gray-900 dark:text-gray-400">
           {selectedSize ? `Chosen size: ${size}` : "Choose size:"}
         </p>
