081ec106e60c564d6005b59a3400b8235e4428da
hellen truong <hellentruong123@gmail.com>
2024-12-13 10:13:24 +0100

    tar bort imports

--- src/pages/Clothes.jsx
+++ src/pages/Clothes.jsx
@@ -1,7 +1,5 @@
-import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { clothesState } from "../states/clothes";
-import { getAllClothes } from "../data/clothes";
 
 import ProductCard from "../components/ProductCard";
 
