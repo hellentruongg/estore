76804799c054ba8e4715989affb62d6ff9261fff
hellen truong <hellentruong123@gmail.com>
2024-12-13 15:12:05 +0100

    visar antal produkter i varukorgen i navigationsfältet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import { Navbar } from "flowbite-react";
+import { Navbar, Badge } from "flowbite-react";
 import { BsCart3 } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 
 import "../styles/NavBar.css";
+import { useRecoilState } from "recoil";
+import { countState } from "../states/count";
 
 export default function NavigationBar() {
+  const [count, setCount] = useRecoilState(countState);
+
   return (
     <>
       <Navbar fluid rounded>
@@ -18,7 +22,9 @@ export default function NavigationBar() {
         <div className="flex md:order-2 list-none">
           <Navbar.Link as={NavLink} to="/cart" className="flex">
             <BsCart3 className="mr-2 h-5 w-5" />
-            Cart
+            <div className="flex rounded-full justify-center items-center -mt-1.5 -ml-3.5 h-4 w-4 bg-red-600 text-white text-xs">
+              {count}
+            </div>
           </Navbar.Link>
 
           <Navbar.Toggle />
